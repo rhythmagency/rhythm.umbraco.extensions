3441013520c7e64c370e2b1d7ace06159e56edc3
Michael Lawrence <michael.lawrence@rhythmagency.com>
2013-09-30 13:51:49 -0700

    Replacing requirejs with browserify. Replacing selectik with select2. Replacing twitter bower with npm.

---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions.Website/App_Plugins/FontAwesomeIconPicker/js/app.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+require('select2/select2');
+
+$(function () {
+	function format (option) {
+		if (!option.id) return option.text; // optgroup
+		return '<i class="' + option.id.toLowerCase() + '"></i> ' + option.text;
+	}
+
+	$('select.umbEditorFontAwesomeIconPicker').select2({
+		'width': 'resolve',
+		'formatResult': format,
+		'formatSelection': format,
+		'escapeMarkup': function (m) { return m; }
+	});
+});
--- trunk/Rhythm.Extensions/Rhythm.Extensions/DataTypes/FontAwesomeIconPicker/DataEditor.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/DataTypes/FontAwesomeIconPicker/DataEditor.cs
@@ -13,21 +13,26 @@ namespace Rhythm.Extensions.DataTypes.FontAwesomeIconPicker {
 
 		private const string FONT_AWESOME_ICON_PICKER_REQUIRE_KEY = "FontAwesomeIconPickerRequire";
 		private const string FONT_AWESOME_ICON_PICKER_CSS_KEY = "FontAwesomeIconPickerCSS";
-		private const string FONT_AWESOME_ICON_PICKER_SELECTIK_CSS_KEY = "FontAwesomeIconPickerSelectikCSS";
+		private const string FONT_AWESOME_ICON_PICKER_SELECT_CSS_KEY = "FontAwesomeIconPickerSelectCSS";
+		private const string FONT_AWESOME_ICON_PICKER_APP_CSS_KEY = "FontAwesomeIconPickerAppCSS";
 
 		protected override void OnInit(EventArgs e) {
 			this.CssClass = "umbEditorFontAwesomeIconPicker";
 
 			if (!this.Page.ClientScript.IsClientScriptIncludeRegistered(FONT_AWESOME_ICON_PICKER_REQUIRE_KEY)) {
-				this.Page.ClientScript.RegisterStartupScript(this.GetType(), FONT_AWESOME_ICON_PICKER_REQUIRE_KEY, "<script src='/App_Plugins/FontAwesomeIconPicker/js/main.min.js'></script>", false);
+				this.Page.ClientScript.RegisterStartupScript(this.GetType(), FONT_AWESOME_ICON_PICKER_REQUIRE_KEY, "<script src='/App_Plugins/FontAwesomeIconPicker/js/app.min.js'></script>", false);
 			}
 
 			if (!this.Page.ClientScript.IsClientScriptIncludeRegistered(FONT_AWESOME_ICON_PICKER_CSS_KEY)) {
 				this.Page.ClientScript.RegisterStartupScript(this.GetType(), FONT_AWESOME_ICON_PICKER_CSS_KEY, "<link rel='stylesheet' href='/App_Plugins/FontAwesomeIconPicker/css/font-awesome.css' />", false);
 			}
 
-			if (!this.Page.ClientScript.IsClientScriptIncludeRegistered(FONT_AWESOME_ICON_PICKER_SELECTIK_CSS_KEY)) {
-				this.Page.ClientScript.RegisterStartupScript(this.GetType(), FONT_AWESOME_ICON_PICKER_SELECTIK_CSS_KEY, "<link rel='stylesheet' href='/App_Plugins/FontAwesomeIconPicker/js/components/selectik/src/selectik.css' />", false);
+			if (!this.Page.ClientScript.IsClientScriptIncludeRegistered(FONT_AWESOME_ICON_PICKER_SELECT_CSS_KEY)) {
+				this.Page.ClientScript.RegisterStartupScript(this.GetType(), FONT_AWESOME_ICON_PICKER_SELECT_CSS_KEY, "<link rel='stylesheet' href='/App_Plugins/FontAwesomeIconPicker/js/node_modules/select2/select2.css' />", false);
+			}
+
+			if (!this.Page.ClientScript.IsClientScriptIncludeRegistered(FONT_AWESOME_ICON_PICKER_APP_CSS_KEY)) {
+				this.Page.ClientScript.RegisterStartupScript(this.GetType(), FONT_AWESOME_ICON_PICKER_APP_CSS_KEY, "<link rel='stylesheet' href='/App_Plugins/FontAwesomeIconPicker/css/app.css' />", false);
 			}
 
 			base.OnInit(e);
