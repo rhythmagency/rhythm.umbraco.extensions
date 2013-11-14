e84f392dd7f635c463dcf649cec6b56cbf4098a7
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2013-11-13 16:25:13 -0800

    Added extension methods to get localized property values. Added Umbraco DLL's and changed references to point to them.

--- trunk/Rhythm.Extensions/Rhythm.Extensions/Constants/DocumentTypes.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Constants/DocumentTypes.cs
@@ -1,5 +1,6 @@
 ﻿namespace Rhythm.Extensions.Constants {
 	public static class DocumentTypes {
 		public const string HOME = "Home";
+        public const string SETTINGS = "Settings";
 	}
 }
--- trunk/Rhythm.Extensions/Rhythm.Extensions/Constants/Properties.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Constants/Properties.cs
@@ -1,5 +1,6 @@
 ﻿namespace Rhythm.Extensions.Constants {
 	public static class Properties {
 		public const string TITLE = "title";
+        public const string VALUE = "value";
 	}
 }
