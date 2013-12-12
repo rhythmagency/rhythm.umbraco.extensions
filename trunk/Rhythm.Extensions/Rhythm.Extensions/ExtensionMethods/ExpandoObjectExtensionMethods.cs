0aed999fa1b4d1d08a05d85b37e9da4be00c256d
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2013-12-12 13:24:06 -0800

    Added ExpandoObjectExtensionMethods.

---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/ExtensionMethods/ExpandoObjectExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Rhythm.Extensions.ExtensionMethods {
+    public static class ExpandoObjectExtensionMethods {
+
+        /// <summary>
+        /// Checks if a property by the specified name exists on the specified object.
+        /// </summary>
+        /// <param name="item">The ExpandoObject.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns>True, if the property exists; otherwise, false.</returns>
+        public static bool HasProperty(this ExpandoObject item, string propertyName) {
+            if (item != null) {
+                if ((item as IDictionary<string, object>).ContainsKey(propertyName)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
