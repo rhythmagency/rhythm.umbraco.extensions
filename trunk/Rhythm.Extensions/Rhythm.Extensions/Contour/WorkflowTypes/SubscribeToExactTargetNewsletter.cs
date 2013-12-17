ea96133a247894c973faa9a39d2c075242536347
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2013-12-16 16:32:13 -0800

    Added NetUtility and SubscribeToExactTargetNewsletter.

---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Contour/WorkflowTypes/SubscribeToExactTargetNewsletter.cs
@@ -0,0 +1,115 @@
+﻿using global::Umbraco.Core.Logging;
+using global::Umbraco.Forms.Core;
+using global::Umbraco.Forms.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Umbraco.Core;
+using NetUtility = Rhythm.Extensions.Utilities.NetUtility;
+using StringPair = System.Collections.Generic.KeyValuePair<string, string>;
+
+namespace Rhythm.Extensions.Contour.WorkflowTypes {
+
+    /// <summary>
+    /// Workflow type that subscribes the user to a newsletter using the Exact Target API.
+    /// </summary>
+    public class SubscribeToExactTargetNewsletter : WorkflowType {
+
+        private const string ErrorPostSubmit = "Error submitting HTTP POST to Exact Target API.";
+        private const string ExactTargetProfileMessage = "Exact Target API call took {0}ms.";
+        private const string ExactTargetSubscribeUrl = "http://cl{0}.exct.net/subscribe.aspx";
+
+        [global::Umbraco.Forms.Core.Attributes.Setting("Exact Target List ID", description = "Enter the list ID", control = "Umbraco.Forms.Core.FieldSetting.TextField")]
+        public string ExactTargetListId { get; set; }
+
+        [global::Umbraco.Forms.Core.Attributes.Setting("Exact Target Instance", description = "Enter the intance (e.g., s1, s2, etc.)", control = "Umbraco.Forms.Core.FieldSetting.TextField")]
+        public string ExactTargetInstance { get; set; }
+
+        [global::Umbraco.Forms.Core.Attributes.Setting("Exact Target Member ID", description = "Enter the member ID", control = "Umbraco.Forms.Core.FieldSetting.TextField")]
+        public string ExactTargetMemberId { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SubscribeToExactTargetNewsletter() {
+            Id = new Guid("81FA16D8-3EE9-4C07-AC85-B4C587DBA9C9");
+            Name = "Subscribe to Newsletter with Exact Target API";
+            Description = "Subscribes the user's email address to the newsletter using the Exact Target API.";
+        }
+
+        /// <summary>
+        /// Subscribes the user to the newsletter.
+        /// </summary>
+        public override WorkflowExecutionStatus Execute(Record record, RecordEventArgs e) {
+
+            // Variables.
+            string email = null;
+            string instancePart = "s1".InvariantEquals(ExactTargetInstance)
+                ? null
+                : "." + ExactTargetInstance;
+            string url = string.Format(ExactTargetSubscribeUrl, instancePart);
+
+            // Gather field values.
+            foreach (var recordField in record.RecordFields.Values) {
+                object recordValue = recordField.Values.FirstOrDefault();
+                string strRecordValue = ((recordValue as string) ?? string.Empty).TrimEnd();
+                switch ((recordField.Field.Caption ?? string.Empty).ToLower()) {
+                    case "email":
+                        email = strRecordValue;
+                        break;
+                    default:
+                        // Ignore the other fields.
+                        break;
+                }
+            }
+
+            // Put data into key/value list.
+            List<StringPair> formData = new List<StringPair>() {
+                new StringPair("MID", ExactTargetMemberId),
+                new StringPair("lid", ExactTargetListId),
+                new StringPair("SubAction", "sub_add_update"),
+                new StringPair("Email Address", email)
+            };
+
+            // Send form.
+            (new System.Threading.Thread(() => {
+                try {
+                    var startTime = DateTime.Now;
+                    var result = NetUtility.GetPostResponse(url, formData);
+                    var deltaTime = DateTime.Now.Subtract(startTime).TotalMilliseconds.ToString();
+                    LogHelper.Info(typeof(SubscribeToExactTargetNewsletter),
+                        () => string.Format(ExactTargetProfileMessage, deltaTime));
+                }
+                catch (Exception ex) {
+                    LogHelper.Error(typeof(SubscribeToExactTargetNewsletter), ErrorPostSubmit, ex);
+                    Thread.ResetAbort();
+                }
+            })).Start();
+
+            // Indicate success.
+            return WorkflowExecutionStatus.Completed;
+
+        }
+
+        /// <summary>
+        /// Validate workflow settings.
+        /// </summary>
+        /// <returns>A list of exceptions detailing any validation errors.</returns>
+        public override List<Exception> ValidateSettings() {
+            var errors = new List<Exception>();
+            if (string.IsNullOrEmpty(ExactTargetListId)) {
+                errors.Add(new Exception("List ID is required."));
+            }
+            if (string.IsNullOrEmpty(ExactTargetInstance)) {
+                errors.Add(new Exception("Instance is required."));
+            }
+            if (string.IsNullOrEmpty(ExactTargetMemberId)) {
+                errors.Add(new Exception("Member ID is required."));
+            }
+            return errors;
+        }
+
+    }
+
+}
---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/NetUtility.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Rhythm.Extensions.Utilities {
+
+    /// <summary>
+    /// Utility to help with network operations.
+    /// </summary>
+    public class NetUtility {
+
+        private const string DEFAULT_USER_AGENT = ".Net Server-Side Client";
+
+        /// <summary>
+        /// Posts a form and gets the response string.
+        /// </summary>
+        /// <param name="url">The URL to post the form data to.</param>
+        /// <param name="data">The form data to post (key/value pairs).</param>
+        /// <returns>The response string.</returns>
+        public static string GetPostResponse(string url, List<KeyValuePair<string, string>> data) {
+            var request = (HttpWebRequest)System.Net.WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.UserAgent = DEFAULT_USER_AGENT;
+            var postData = new StringBuilder();
+            string ampersand = string.Empty;
+            foreach (var pair in data)
+            {
+                postData.Append(ampersand);
+                postData.Append(pair.Key).Append("=");
+                postData.Append(WebUtility.UrlEncode(pair.Value));
+                ampersand = "&";
+            }
+            Encoding encoding = Encoding.UTF8;
+            byte[] postBytes = encoding.GetBytes(postData.ToString());
+            request.ContentLength = postBytes.Length;
+            using (Stream postStream = request.GetRequestStream()) {
+                postStream.Write(postBytes, 0, postBytes.Length);
+                postStream.Flush();
+                postStream.Close();
+            }
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            var reader = new StreamReader(responseStream);
+            return reader.ReadToEnd();
+        }
+
+    }
+
+}
