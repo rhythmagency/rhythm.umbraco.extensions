7c2278fee28abefbae40683dc508a4f7c4509214
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2013-12-30 19:21:05 -0800

    Added NetUtility.GetResponse().

--- trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/NetUtility.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/NetUtility.cs
@@ -46,6 +46,21 @@ namespace Rhythm.Extensions.Utilities {
             return reader.ReadToEnd();
         }
 
+
+        /// <summary>
+        /// Returns the response string downloaded from the request to the specified URL.
+        /// </summary>
+        /// <param name="url">The URL to make the request to.</param>
+        /// <returns>The response string.</returns>
+        public static string GetResponse(string url) {
+            var request = (HttpWebRequest)System.Net.WebRequest.Create(url);
+            request.UserAgent = DEFAULT_USER_AGENT;
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            var reader = new StreamReader(responseStream);
+            return reader.ReadToEnd();
+        }
+
     }
 
 }
