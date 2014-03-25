fe5aa1222adc481a9320f6123743c8eae2697d5c
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2014-03-25 14:48:45 -0700

    New regular expression patterns. Added ValueLabelPair.

--- trunk/Rhythm.Extensions/Rhythm.Extensions/Constants/RegexPatterns.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Constants/RegexPatterns.cs
@@ -1,5 +1,12 @@
 ﻿namespace Rhythm.Extensions.Constants {
 	public static class RegexPatterns {
+        /// <summary>
+        /// If validating an MVC model, consider using these attributes instead:
+        ///     [DataType(DataType.EmailAddress)]
+        ///     [EmailAddress()]
+        /// </summary>
 		public const string EmailAddress = @"^.+@.+\..+$";
+        public const string Date = @"^(0?[1-9]|10|11|12)/(0?[1-9]|1[0-9]|2[0-9]|3[0-1])/[0-9]{4}$";
+        public const string Phone = @"^(.*[0-9]){7}.*$";
 	}
 }
---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Types/ValueLabelPair.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Rhythm.Extensions.Types {
+	
+	/// <summary>
+	/// Defines a value/label pair that can be set or retrieved.
+	/// </summary>
+	/// <typeparam name="TValue">The type of the value.</typeparam>
+	/// <typeparam name="TLabel">The type of the label.</typeparam>
+	[Serializable]
+	public struct ValueLabelPair<TValue, TLabel> {
+		
+		private readonly TValue value;
+		private readonly TLabel label;
+		
+		/// <summary>
+		/// The value.
+		/// </summary>
+		public TValue Value {
+			get {
+				return value;
+			}
+		}
+		
+		/// <summary>
+		/// The label.
+		/// </summary>
+		public TLabel Label {
+			get {
+				return label;
+			}
+		}
+		
+		/// <summary>
+		/// Full constructor.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="label">The label.</param>
+		public ValueLabelPair(TValue value, TLabel label) {
+			this.value = value;
+			this.label = label;
+		}
+		
+	}
+	
+}
