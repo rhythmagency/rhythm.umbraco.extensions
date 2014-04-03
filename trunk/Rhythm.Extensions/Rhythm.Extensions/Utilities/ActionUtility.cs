aa17d7eb5fa14d99bcd09d73d936c91796e33771
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2014-04-03 16:55:28 -0700

    Added ActionUtility.

---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/ActionUtility.cs
@@ -0,0 +1,107 @@
+﻿namespace Rhythm.Extensions.Utilities {
+
+	// Namespaces.
+	using System;
+	using System.Threading;
+
+	/// <summary>
+	/// Utility to help with actions.
+	/// </summary>
+	public class ActionUtility {
+
+		/// <summary>
+		/// Attempts to execute an action in the specified time limit,
+		/// optionally executing an alternate task on failure.
+		/// </summary>
+		/// <param name="action">The action to attempt to execute.</param>
+		/// <param name="limit">The duration of time the action is allowed to execute for.</param>
+		/// <param name="alternate">
+		/// Optional.
+		/// The alternate action to execute if the first one fails to execute in the given time limit.
+		/// </param>
+		/// <returns>
+		/// True, if the action executed within the given time limit; otherwise, false.
+		/// </returns>
+		/// <remarks>
+		/// If the action does not execute within the given time limit, it will be aborted.
+		/// </remarks>
+		public static bool Attempt(Action action, TimeSpan limit, Action alternate = null) {
+
+			// Validate input.
+			if (action == null) {
+				if (alternate != null) {
+					alternate();
+				}
+				return false;
+			}
+			else {
+
+				// Variables.
+				Exception exception = null;
+				object hadException = false;
+				object requestingAbort = false;
+
+				// Wrap in a thread so the duration can be limited.
+				Thread t = new Thread(new ThreadStart(() => {
+
+					// Attempt to execute the action.
+					try {
+						action();
+					}
+					catch (ThreadAbortException ex) {
+
+						// Avoid thread abort exceptions.
+						if ((bool)requestingAbort) {
+							try {
+								Thread.ResetAbort();
+							}
+							catch { }
+						}
+						else {
+							exception = ex;
+							hadException = true;
+						}
+
+					}
+					catch (Exception ex) {
+
+						// Save other exceptions.
+						exception = ex;
+						hadException = true;
+
+					}
+
+				}));
+				
+				// Try to execute the thread within the time limit.
+				t.Start();
+				var success = t.Join(limit);
+				if (success) {
+
+					// Propagate exceptions.
+					if ((bool)hadException) {
+						throw exception;
+					}
+
+					// Success.
+					return true;
+
+				}
+				else {
+
+					// Abort thread and try alternate.
+					requestingAbort = true;
+					t.Abort();
+					if (alternate != null) {
+						alternate();
+					}
+					return false;
+
+				}
+
+			}
+		}
+
+	}
+
+}
