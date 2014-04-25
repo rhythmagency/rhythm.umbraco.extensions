1d322f38bf4622e7f16104f4c5b8e9f30ce35111
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2014-04-25 14:25:51 -0700

    Added workaround to RhythmModelBinder to avoid errors on non-null array properties on models (caused by an MVC limitation).

--- trunk/Rhythm.Extensions/Rhythm.Extensions/Binding/RhythmModelBinder.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Binding/RhythmModelBinder.cs
@@ -2,6 +2,7 @@
 
 	// Namespaces.
 	using Attributes;
+	using System;
 	using System.Web.Mvc;
 
 	/// <summary>
@@ -15,6 +16,18 @@
 	public class RhythmModelBinder : DefaultModelBinder {
 		protected override void BindProperty(System.Web.Mvc.ControllerContext controllerContext,
 			ModelBindingContext bindingContext, System.ComponentModel.PropertyDescriptor propertyDescriptor) {
+			
+			// Workaround for MVC limitation/bug.
+			// See: http://blog.baltrinic.com/software-development/dotnet/better-array-model-binding-in-asp-net-mvc
+			// Essentially, any non-null array property on a model will throw an exception
+			// when using the default model binder. This workaround forces empty arrays to be null.
+			if (propertyDescriptor.PropertyType.BaseType == typeof(Array)) {
+				var propertyArray = propertyDescriptor.GetValue(bindingContext.Model);
+				if (propertyArray is Array && (propertyArray as Array).Length == 0)
+				{
+					propertyDescriptor.SetValue(bindingContext.Model, null);
+				}
+			}
 
 			// Default binding.
 			base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
