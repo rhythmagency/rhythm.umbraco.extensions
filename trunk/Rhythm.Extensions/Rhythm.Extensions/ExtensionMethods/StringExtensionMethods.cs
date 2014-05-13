5e0a54a4634a676914e74a437b7ccb13863b5bdd
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2014-05-13 16:36:27 -0700

    Added BlankToNull() to string extensions.

--- trunk/Rhythm.Extensions/Rhythm.Extensions/ExtensionMethods/StringExtensionMethods.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/ExtensionMethods/StringExtensionMethods.cs
@@ -3,6 +3,12 @@ using System.Text;
 
 namespace Rhythm.Extensions.ExtensionMethods {
 	public static class StringExtensionMethods {
+
+		/// <summary>
+		/// Creates an MD5 hash for a string.
+		/// </summary>
+		/// <param name="input">The input string.</param>
+		/// <returns>The MD5 hash.</returns>
 		public static string ToMd5Hash(this string input) {
 			var md5 = MD5.Create();
 			var inputBytes = Encoding.ASCII.GetBytes(input);
@@ -15,5 +21,21 @@ namespace Rhythm.Extensions.ExtensionMethods {
 
 			return sb.ToString();
 		}
+
+		/// <summary>
+		/// Returns null of the input is only whitespace.
+		/// </summary>
+		/// <param name="input">The input string.</param>
+		/// <returns>
+		/// The original string, or null if the original string is whitespace or an empty string.
+		/// </returns>
+		/// <remarks>
+		/// This is useful when combined with the null coalescing operator.
+		/// </remarks>
+		public static string BlankToNull(this string input)
+		{
+			return string.IsNullOrWhiteSpace(input) ? null : input;
+		}
+
 	}
 }
