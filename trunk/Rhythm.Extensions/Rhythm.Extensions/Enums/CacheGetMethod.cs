67b07783172002c7161121f2c61e16f88eb37d9d
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2014-07-15 09:55:28 -0700

    Added CacheGetMethod to facilitate alternative ways of interacting with the cache.

---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Enums/CacheGetMethod.cs
@@ -0,0 +1,34 @@
+﻿namespace Rhythm.Extensions.Enums
+{
+
+	/// <summary>
+	/// The methods that can be used when retrieving values from a cache.
+	/// </summary>
+	public enum CacheGetMethod
+	{
+
+		/// <summary>
+		/// Gets a value from the cache, replenishing the cache with a new value
+		/// if necessary.
+		/// </summary>
+		Default,
+
+		/// <summary>
+		/// Gets a value from the cache, or gets a new value, but will not store
+		/// the new value to the cache.
+		/// </summary>
+		NoStore,
+
+		/// <summary>
+		/// Gets a new value each time and does not store it to the cache.
+		/// </summary>
+		NoCache,
+
+		/// <summary>
+		/// Gets a new value and stores the result to the cache.
+		/// </summary>
+		Recache
+
+	}
+
+}
--- trunk/Rhythm.Extensions/Rhythm.Extensions/Types/InstanceCache.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Types/InstanceCache.cs
@@ -1,55 +1,69 @@
 ﻿namespace Rhythm.Extensions.Types {
 
-    // Namespaces.
-    using System;
+	// Namespaces.
+	using Enums;
+	using System;
 
-    /// <summary>
-    /// Caches and instance variable.
-    /// </summary>
-    /// <typeparam name="T">The type of variable to cache.</typeparam>
-    public class InstanceCache<T> {
+	/// <summary>
+	/// Caches and instance variable.
+	/// </summary>
+	/// <typeparam name="T">The type of variable to cache.</typeparam>
+	public class InstanceCache<T> {
 
-        #region Properties
+		#region Properties
 
-        private DateTime? LastCache { get; set; }
-        private T Instance { get; set; }
-        private object InstanceLock { get; set; }
+		private DateTime? LastCache { get; set; }
+		private T Instance { get; set; }
+		private object InstanceLock { get; set; }
 
-        #endregion
+		#endregion
 
-        #region Constructors
+		#region Constructors
 
-        /// <summary>
-        /// Default constructor.
-        /// </summary>
-        public InstanceCache() {
-            InstanceLock = new object();
-            LastCache = null;
-        }
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public InstanceCache() {
+			InstanceLock = new object();
+			LastCache = null;
+		}
 
-        #endregion
+		#endregion
 
-        #region Methods
+		#region Methods
 
-        /// <summary>
-        /// Gets the instance variable (either from the cache or from the specified function).
-        /// </summary>
-        /// <param name="duration">The duration to cache for.</param>
-        /// <param name="replenisher">The function that replenishes the cache.</param>
-        /// <returns>The instance variable.</returns>
-        public T Get(TimeSpan duration, Func<T> replenisher) {
-            lock (InstanceLock) {
-                var now = DateTime.Now;
-                if (!LastCache.HasValue || now.Subtract(LastCache.Value) >= duration) {
-                    Instance = replenisher();
-                    LastCache = now;
-                }
-                return Instance;
-            }
-        }
+		/// <summary>
+		/// Gets the instance variable (either from the cache or from the specified function).
+		/// </summary>
+		/// <param name="duration">The duration to cache for.</param>
+		/// <param name="replenisher">The function that replenishes the cache.</param>
+		/// <param name="method">Optional. The cache method to use when retrieving the value.</returns>
+		public T Get(TimeSpan duration, Func<T> replenisher,
+			CacheGetMethod method = CacheGetMethod.Default) {
+				if (method == CacheGetMethod.NoCache) {
+					return replenisher();
+				} else {
+					lock (InstanceLock) {
+						var now = DateTime.Now;
+						if (method == CacheGetMethod.Recache) {
+							Instance = replenisher();
+							LastCache = now;
+						}
+						else if (!LastCache.HasValue || now.Subtract(LastCache.Value) >= duration) {
+							if (method == CacheGetMethod.NoStore) {
+								return replenisher();
+							} else {
+								Instance = replenisher();
+								LastCache = now;
+							}
+						}
+						return Instance;
+					}
+				}
+		}
 
-        #endregion
+		#endregion
 
-    }
+	}
 
 }
