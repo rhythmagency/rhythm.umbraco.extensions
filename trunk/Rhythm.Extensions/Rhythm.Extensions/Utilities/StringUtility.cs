01a0996d572c7a798207a743656f920753d4d7a4
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2014-07-21 14:37:30 -0700

    Added some methods to StringUtility (RenderMarkupToText and Truncate).

--- trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/StringUtility.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/StringUtility.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 
 namespace Rhythm.Extensions.Utilities {
@@ -7,6 +10,12 @@ namespace Rhythm.Extensions.Utilities {
 	/// Utility to help with strings.
 	/// </summary>
 	public class StringUtility {
+
+		/// <summary>
+		/// The regex to find sequences of whitespace (so they can be
+		/// replaced with a single space).
+		/// </summary>
+		private static Regex WhitespaceRegex { get; set; }
 
 		/// <summary>
 		/// The regex to find invalid characters in string (so they can be
@@ -20,6 +29,7 @@ namespace Rhythm.Extensions.Utilities {
 		static StringUtility()
 		{
 			var defaultOptions = RegexOptions.Compiled | RegexOptions.IgnoreCase;
+			WhitespaceRegex = new Regex("( |\t|\r|\n)+", defaultOptions);
 			InvalidClassChars = new Regex(@"((?![a-z0-9_])(.|\r|\n))+", defaultOptions);
 		}
 
@@ -52,6 +62,70 @@ namespace Rhythm.Extensions.Utilities {
 		public static string ToCssClass(string value) {
 			value = InvalidClassChars.Replace((value ?? string.Empty), "-").ToLower();
 			return value;
+		}
+
+		/// <summary>
+		/// Gets the text that would be displayed to a user of a web browser.
+		/// </summary>
+		/// <param name="markup">The markup.</param>
+		/// <returns>The text.</returns>
+		public static string RenderMarkupToText(string markup) {
+			
+			// Validation.
+			if (string.IsNullOrWhiteSpace(markup)) {
+				return markup;
+			}
+
+			// Variables.
+			var text = null as string;
+
+			// Remove HTML commments.
+			var doc = new HtmlDocument();
+			doc.LoadHtml(markup);
+			doc.DocumentNode.DescendantsAndSelf().Where(x => x.NodeType == HtmlNodeType.Comment)
+				.ToList().ForEach(x => x.Remove());
+
+			// Get text.
+			if (doc.DocumentNode == null) {
+				text = string.Empty;
+			} else {
+				text = (doc.DocumentNode.InnerText ?? string.Empty).Trim();
+			}
+
+			// Unencode HTML entities.
+			text = WebUtility.HtmlDecode(text) ?? string.Empty;
+
+			// Reduce whitespace.
+			text = (WhitespaceRegex.Replace(text, " ") ?? string.Empty).Trim();
+
+			// Return text.
+			return text;
+
+		}
+
+		/// <summary>
+		/// Truncates a string, adding an ellipsis when necessary.
+		/// </summary>
+		/// <param name="str">The string to truncate.</param>
+		/// <param name="maxLength">The maximum allowed length.</param>
+		/// <param name="ellipsis">
+		/// The ellipsis (can be any string to suffix after truncating).
+		/// </param>
+		/// <returns>The truncated string.</returns>
+		/// <remarks>
+		/// The ellipsis will only be suffixed if truncation is performed.
+		/// The resulting string will not be longer than the max length,
+		/// including the ellipsis.
+		/// </remarks>
+		public static string Truncate(string str, int maxLength, string ellipsis) {
+			if (!string.IsNullOrWhiteSpace(str) && str.Length > maxLength) {
+				var newLength = Math.Max(0, maxLength - ellipsis.Length);
+				str = str.Substring(0, newLength).TrimEnd() + ellipsis;
+				if (str.Length > maxLength) {
+					str = str.Substring(0, maxLength);
+				}
+			}
+			return str;
 		}
 
 	}
