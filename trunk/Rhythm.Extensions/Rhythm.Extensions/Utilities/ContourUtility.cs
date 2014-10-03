42437bc050a3a12070658fc5ee3439c3acea8645
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2014-10-03 11:05:05 -0700

    Created an overload for ContourUtility.StoreRecord that accepts a form GUID.

--- trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/ContourUtility.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/ContourUtility.cs
@@ -37,7 +37,7 @@
 
 		#endregion
 
-		#region Methods
+		#region Public Methods
 
 		/// <summary>
 		/// Gets the field values for the specified field on the specified form.
@@ -101,6 +101,58 @@
 			var success = true;
 
 			// Ensure keys are lowercase.
+			fieldValues = GetLowercaseKeys(fieldValues);
+
+			// Add record.
+			using (var formStorage = new FormStorage()) {
+				var form = formStorage.GetForm(formName);
+				success = success && StoreRecord(form, fieldValues, ipAddress, pageId);
+			}
+
+			// Succeeded?
+			return success;
+
+		}
+
+		/// <summary>
+		/// Stores a form submission to Contour.
+		/// </summary>
+		/// <param name="formGuid">The GUID of the form.</param>
+		/// <param name="fieldValues">The values to store to contour.</param>
+		/// <param name="ipAddress">The user's IP address.</param>
+		/// <param name="pageId">The ID of the page the user entered the values on.</param>
+		/// <returns>True, if the data was stored successfully; otherwise, false.</returns>
+		public static bool StoreRecord(Guid formGuid, Dictionary<string, List<object>> fieldValues,
+			string ipAddress, int pageId) {
+
+			// Variables.
+			var success = true;
+
+			// Ensure keys are lowercase.
+			fieldValues = GetLowercaseKeys(fieldValues);
+
+			// Add record.
+			using (var formStorage = new FormStorage()) {
+				var form = formStorage.GetForm(formGuid);
+				success = success && StoreRecord(form, fieldValues, ipAddress, pageId);
+			}
+
+			// Succeeded?
+			return success;
+
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Converts a dictionary of values so each key is lowercase.
+		/// </summary>
+		/// <param name="fieldValues">The original dictionary.</param>
+		/// <returns>The dictionary with lowercase keys.</returns>
+		private static Dictionary<string,List<object>> GetLowercaseKeys(
+			Dictionary<string, List<object>> fieldValues) {
 			fieldValues = new Dictionary<string,List<object>>(fieldValues);
 			var oldKeys = fieldValues.Keys;
 			foreach (var key in oldKeys) {
@@ -111,66 +163,70 @@
 					fieldValues[lowerKey] = value;
 				}
 			}
+			return fieldValues;
+		}
 
-			// Add record.
-			using (var formStorage = new FormStorage()) {
-				var form = formStorage.GetForm(formName);
-				using (var recordStorage = new RecordStorage())
-				using (var recordService = new RecordService(form)) {
+		/// <summary>
+		/// Stores a form record.
+		/// </summary>
+		/// <param name="form">The form to store a record to.</param>
+		/// <param name="fieldValues">The field values to store.</param>
+		/// <param name="ipAddress">The user's IP address.</param>
+		/// <param name="pageId">The current page ID.</param>
+		/// <returns>True, if the record was stored; otherwise, false.</returns>
+		private static bool StoreRecord(Form form, Dictionary<string, List<object>> fieldValues,
+			string ipAddress, int pageId) {
+			using (var recordStorage = new RecordStorage())
+			using (var recordService = new RecordService(form)) {
 
-					// Open record service.
-					recordService.Open();
+				// Open record service.
+				recordService.Open();
 
-					// Create record.
-					var record = recordService.Record;
-					record.IP = ipAddress;
-					record.UmbracoPageId = pageId;
-					recordStorage.InsertRecord(record, form);
+				// Create record.
+				var record = recordService.Record;
+				record.IP = ipAddress;
+				record.UmbracoPageId = pageId;
+				recordStorage.InsertRecord(record, form);
 
-					// Assign field values for record.
-					foreach (var field in recordService.Form.AllFields) {
+				// Assign field values for record.
+				foreach (var field in recordService.Form.AllFields) {
 
-						// Check which field this is.
-						bool knownField = false;
-						List<object> values = null;
-						if (fieldValues.TryGetValue((field.Caption ?? string.Empty).ToLower(), out values)) {
-							knownField = true;
+					// Check which field this is.
+					var knownField = false;
+					var values = default(List<object>);
+					if (fieldValues.TryGetValue((field.Caption ?? string.Empty).ToLower(), out values)) {
+						knownField = true;
+					}
+
+					// If the field is one of those that are known, store it.
+					if (knownField && values != null && values.Any(x => x != null)) {
+
+						// Create field.
+						var key = Guid.NewGuid();
+						var recordField = new RecordField {
+							Field = field,
+							Key = key,
+							Record = record.Id,
+							Values = values
+						};
+
+						// Store field.
+						using (var recordFieldStorage = new RecordFieldStorage()) {
+							recordFieldStorage.InsertRecordField(recordField);
 						}
 
-						// If the field is one of those that are known, store it.
-						if (knownField && values != null && values.Any(x => x != null)) {
-
-							// Create field.
-							var key = Guid.NewGuid();
-							var recordField = new RecordField {
-								Field = field,
-								Key = key,
-								Record = record.Id,
-								Values = values
-							};
-
-							// Store field.
-							using (var recordFieldStorage = new RecordFieldStorage()) {
-								recordFieldStorage.InsertRecordField(recordField);
-							}
-
-							// Add field to record.
-							record.RecordFields.Add(key, recordField);
-
-						}
+						// Add field to record.
+						record.RecordFields.Add(key, recordField);
 
 					}
 
-					// Submit / save record.
-					recordService.Submit();
-					success = success && recordService.SaveFormToRecord();
-
 				}
+
+				// Submit / save record.
+				recordService.Submit();
+				return recordService.SaveFormToRecord();
+
 			}
-
-			// Succeeded?
-			return success;
-
 		}
 
 		#endregion
