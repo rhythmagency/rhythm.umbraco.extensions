c80a4f64ff52d74ede8b8783c11f3b1fc215b445
Danny Rooks <drooks@gmail.com>
2016-04-12 17:28:59 -0700

    Add InstanceByKeyCacheEx with new test project

---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions.Test/InstanceByKeyCacheEx.Test.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhythm.Extensions.Types;
+using System.Threading;
+
+namespace Rhythm.Extensions.Test
+{
+    /// <summary>
+    /// Summary description for InstanceByKeyCacheEx
+    /// </summary>
+    [TestClass]
+    public class InstanceByKeyCacheExTest
+    {
+        public InstanceByKeyCacheExTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        public sealed class Client
+        {
+            private List<string> FirstNames = new List<string>() { "Lina", "Kristeen", "Lenita", "Aurore", "Euna", "Tanisha", "Mia", "Alfonso", "Jonelle", "Sade", "Dale", "Roy", "Halina", "Ivy", "Jettie", "Darrick", "Felica", "Krystal", "Rolanda", "Brittaney", "Marie", "Zetta", "Miyoko", "Iluminada", "Rosaura", "Carry", "Audrey", "Milan", "Arnoldo", "Romaine", "Detra", "Darrell", "Keshia", "Renita", "Carla", "Bryce", "Angela", "Charissa", "Megan", "Genna", "Barbar", "Ciera", "Junior", "Reina", "Karlene", "Judie", "Rusty", "Lawanna", "Ja", "Justin", "Beau", "Kenya", "Alene", "Ashleigh", "Librada", "Michiko", "Verline", "Felicia", "Silvana", "Kelli", "Suzi", "Cicely", "Nicki", "Sheryll", "Jacalyn", "Helene", "Natalia", "Joey", "Malorie", "Betsy", "Soraya", "Carola", "Joette", "Weldon", "Fran", "Harley", "Keith", "Zoila", "Simon", "Maggie", "Ed", "Ethan", "Yvone", "Gregg", "Deloise", "Demetrius", "Ilana", "Gwenda", "Esteban", "Luba", "Lashanda", "Milly", "Bonita", "Aimee", "Louann", "Tilda", "Eulah", "Joslyn", "Cameron" };
+
+            public Client()
+            {
+                Random rs = new Random((int)DateTime.Now.Ticks);
+                FirstName = FirstNames[rs.Next() % FirstNames.Count];
+                LastName = FirstNames[rs.Next() % FirstNames.Count];
+            }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        [TestMethod]
+        public void TestCache()
+        {
+            //Create a Cache instance with an expiration time and replenisher function
+            // Replenisher returns a new client with random first and last name
+            var cache = new InstanceByKeyCacheEx<Client, string>(new TimeSpan(0, 0, 10), (x) =>
+            {
+                return new Client(); // returns random named client on each call
+            });
+
+            var key1 = "MyKey";
+
+            var clientOrig = cache.Get(key1);
+            var clientNew = cache.Get(key1, Enums.CacheGetMethod.NoCache); // gets replenisher result
+
+            Assert.AreNotSame(clientOrig, clientNew, "Unless a random name was generated twice client2 should be different");
+
+            clientNew = cache.Get(key1, Enums.CacheGetMethod.FromCache); // gets cache or default regardless if expired
+            Assert.AreSame(clientOrig, clientNew, "These object should be the same but FromCache generated default(T)");
+
+            clientNew = cache.Get(key1, Enums.CacheGetMethod.NoStore); // gets cache if not expired, else gets replenisher result
+            Assert.AreSame(clientOrig, clientNew, "Assume the cache hasn't expired, These object should be the same but NoStore fetched a new Client");
+
+            Thread.Sleep(new TimeSpan(0, 0, 11)); // sit and let the cache expire
+
+            clientNew = cache.Get(key1, Enums.CacheGetMethod.NoStore);  // gets cache if not expired, else gets replenisher result
+            Assert.AreNotSame(clientOrig, clientNew, "Assume the cache has expired, These object should not be the same but FromCache fetched an identical client");
+            //Assert.AreEqual(clientOrig, client2, "Assume the cache has expired, These object should not be the same but FromCache fetched an identical client");
+        }
+    }
+}
---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Types/InstanceByKeyCacheEx.cs
@@ -0,0 +1,151 @@
+﻿namespace Rhythm.Extensions.Types
+{
+
+    // Namespaces.
+    using Enums;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Caches a collection of instance variables by key (similar to a dictionary).
+    /// </summary>
+    /// <typeparam name="T">The type of variable to cache.</typeparam>
+    /// <typeparam name="TKey">The type of key to cache by.</typeparam>
+    public class InstanceByKeyCacheEx<T, TKey>
+    {
+        #region Properties
+
+        private Dictionary<TKey, Tuple<T, DateTime>> Instances { get; set; }
+        private object InstancesLock { get; set; }
+        private TimeSpan TimeToLive { get; set; }
+        private Func<TKey, T> Replenisher { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public InstanceByKeyCacheEx()
+        {
+            InstancesLock = new object();
+            Instances = new Dictionary<TKey, Tuple<T, DateTime>>();
+            TimeToLive = new TimeSpan(1,0,0); // default cache ttl to be 1 hour
+            var methodInfo = GetType().GetProperty("Prop").GetGetMethod();
+        }
+
+        /// <summary>
+        /// Constructor object with TimeToLive to define an item duration
+        /// </summary>
+        /// <param name="timeToLive"></param>
+        /// <param name="replenisher"></param>
+        public InstanceByKeyCacheEx(TimeSpan timeToLive, Func<TKey, T> replenisher)
+        {
+            InstancesLock = new object();
+            Instances = new Dictionary<TKey, Tuple<T, DateTime>>();
+            TimeToLive = timeToLive;
+            Replenisher = replenisher;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Using TimeToLive, determine if the instance should be fetched
+        /// </summary>
+        /// <returns></returns>
+        public bool IsCacheExpired(Tuple<T, DateTime> instance)
+        {
+            return instance.Item2.Add(TimeToLive) < DateTime.Now;
+        }
+
+        /// <summary>
+        /// Gets the instance variable (either from the cache or from the specified function).
+        /// </summary>
+        /// <param name="key">The key to use when fetching the variable.</param>
+        /// <param name="replenisher">The function that replenishes the cache.</param>
+        /// <param name="method">Optional. The cache method to use when retrieving the value.</param>
+        public T Get(TKey key, CacheGetMethod method = CacheGetMethod.Default)
+        {
+
+            var now = DateTime.Now;
+            var tempInstance = default(T);
+            var tempTuple = default(Tuple<T, DateTime>);
+
+            lock (InstancesLock)
+
+                switch (method)
+                {
+                    case CacheGetMethod.NoCache:
+                        return Replenisher(key);
+
+                    case CacheGetMethod.FromCache:
+                        // Attempt to get instance from cache without replenishing.
+                        if (Instances.TryGetValue(key, out tempTuple))
+                        {
+                            return tempTuple.Item1;
+                        }
+                        else
+                        {
+                            return default(T);
+                        }
+
+                    case CacheGetMethod.Recache:
+                        tempInstance = Replenisher(key);
+                        Instances[key] = new Tuple<T, DateTime>(tempInstance, now);
+                        return tempInstance;
+
+                    case CacheGetMethod.Default:
+                    case CacheGetMethod.NoStore:
+                        if (Instances.TryGetValue(key, out tempTuple) && !IsCacheExpired(tempTuple))
+                        {
+                            // Cache still valid. Use cached value.
+                            return tempTuple.Item1;
+                        }
+                        else
+                        {
+                            tempInstance = Replenisher(key);
+                            // Cache expired. Get a new value without modifying the cache.
+                            if (method == CacheGetMethod.Default)
+                            {
+                                Instances[key] = new Tuple<T, DateTime>(tempInstance, now);
+                            }
+                            return tempInstance;
+                        }
+
+                    default:
+                        return default(T);
+                }
+        }
+
+        /// <summary>
+        /// Clears the cache.
+        /// </summary>
+        public void Clear()
+        {
+            lock (InstancesLock)
+            {
+                Instances.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Clears the cache of the specified keys.
+        /// </summary>
+        /// <param name="keys">The keys to clear the cache of.</param>
+        public void ClearKeys(IEnumerable<TKey> keys)
+        {
+            lock (InstancesLock)
+            {
+                foreach (var key in keys)
+                {
+                    Instances.Remove(key);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
