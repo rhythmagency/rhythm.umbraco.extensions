d9bed5248b68bfb7f1bee798c0643d7b93921eed
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-04-26 12:50:35 -0700

    Added parse utility.

---
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Utilities/ParseUtility.cs
@@ -0,0 +1,33 @@
+﻿namespace Rhythm.Extensions.Utilities {
+
+	/// <summary>
+	/// Utility to assist with parsing.
+	/// </summary>
+	public class ParseUtility {
+
+		#region Methods
+
+		/// <summary>
+		/// Attempts to parse an integer.
+		/// </summary>
+		/// <param name="str">
+		/// The string to parse.
+		/// </param>
+		/// <returns>
+		/// The parsed integer, or null.
+		/// </returns>
+		public static int? AttemptParseInt(string str) {
+			var value = default(int);
+			if (int.TryParse(str, out value)) {
+				return value;
+			}
+			else {
+				return null;
+			}
+		}
+
+		#endregion
+
+	}
+
+}
