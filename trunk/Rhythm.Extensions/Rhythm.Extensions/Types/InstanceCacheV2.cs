0499144a1caf7bdf308c729ba3b39b43f3b7de21
Andy Jenkins <andy.jenkins@rhythmagency.com>
2018-04-19 13:03:27 -0700

    Added V2 of the instance cache, moving configuration into the constructor.

--- trunk/Rhythm.Extensions/Rhythm.Extensions/Types/InstanceCacheV2.cs
+++ trunk/Rhythm.Extensions/Rhythm.Extensions/Types/InstanceCacheV2.cs
@@ -9,7 +9,11 @@
 	/// Caches and instance variable.
 	/// </summary>
 	/// <typeparam name="T">The type of variable to cache.</typeparam>
-	public class InstanceCache<T> {
+    /// <remarks>
+    /// Compared to the original InstanceCache, this version moves the retrieval parameters into
+    /// the constructor, so that the class can have more awareness of its state.
+    /// </remarks>
+	public class InstanceCacheV2<T> {
 
 		#region Properties
 
@@ -17,51 +21,68 @@
 		private T Instance { get; set; }
 		private object InstanceLock { get; set; }
 
-		#endregion
+        private TimeSpan Duration { get; set; }
+        private Func<T> Replenisher { get; set; }
+		private CacheGetMethod Method { get; set; }
 
-		#region Constructors
+        #endregion
 
-		/// <summary>
-		/// Default constructor.
-		/// </summary>
-		public InstanceCache() {
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public InstanceCacheV2(
+            TimeSpan duration,
+            Func<T> replenisher,
+            CacheGetMethod method = CacheGetMethod.Default
+        ) {
 			InstanceLock = new object();
 			LastCache = null;
+
+            Duration = duration;
+            Replenisher = replenisher;
+            Method = method;
 		}
 
         #endregion
 
         #region Methods
+
+        public bool HasValue {
+            get { return LastCache.HasValue; }
+        }
+
+        public bool IsExpired {
+            get { return LastCache.HasValue && (DateTime.Now - LastCache.Value) > Duration; }
+        }
+
 		/// <summary>
 		/// Gets the instance variable (either from the cache or from the specified function).
 		/// </summary>
 		/// <param name="duration">The duration to cache for.</param>
 		/// <param name="replenisher">The function that replenishes the cache.</param>
 		/// <param name="method">Optional. The cache method to use when retrieving the value.</returns>
-		public T Get(
-			TimeSpan duration,
-			Func<T> replenisher,
-			CacheGetMethod method = CacheGetMethod.Default
-		) {
-			if (method == CacheGetMethod.FromCache) {
+		public T Get() {
+			if (Method == CacheGetMethod.FromCache) {
 				lock (InstanceLock) {
 					return LastCache.HasValue ? Instance : default(T);
 				}
-			} else if (method == CacheGetMethod.NoCache) {
-				return replenisher();
+			} else if (Method == CacheGetMethod.NoCache) {
+				return Replenisher();
 			} else {
 				lock (InstanceLock) {
 					var now = DateTime.Now;
-					if (method == CacheGetMethod.Recache) {
-						Recache(replenisher);
+					if (Method == CacheGetMethod.Recache) {
+						Recache(Replenisher);
 					}
-					else if (!LastCache.HasValue || now.Subtract(LastCache.Value) >= duration) {
-						if (method == CacheGetMethod.NoStore) {
-							return replenisher();
-						} else if (method == CacheGetMethod.DefaultRecacheAfter && LastCache.HasValue) {
-							DeferredRecache(replenisher);
+					else if (!LastCache.HasValue || now.Subtract(LastCache.Value) >= Duration) {
+						if (Method == CacheGetMethod.NoStore) {
+							return Replenisher();
+						} else if (Method == CacheGetMethod.DefaultRecacheAfter && LastCache.HasValue) {
+							DeferredRecache(Replenisher);
 						} else {
-							Recache(replenisher);
+							Recache(Replenisher);
 						}
 					}
 					return Instance;
